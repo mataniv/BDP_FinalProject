4ee42b0abf05a06713b258e37fc50ec880cf7460
innerproduct97 <matanniv123@gmail.com>
2024-02-14 12:09:02 +0200

    remove old files

--- ingestion/ingestion.py
+++ ingestion/ingestion.py
@@ -1,90 +1,93 @@
 import datetime
-import numpy as np
 from flask import Flask, request, jsonify
 import pandas as pd
-import mysql.connector
+from cassandra.cluster import Cluster
+from cassandra.query import SimpleStatement
 
 app = Flask(__name__)
 
-# MySQL database credentials
-# mysql_host = 'mysql-service'  # Replace with your MySQL service name in Kubernetes
-mysql_host = 'localhost'  # Replace with your MySQL service name in Kubernetes
-mysql_user = 'root'
-mysql_password = 'root'
-mysql_database = 'twitter_data'
+# Cassandra database credentials
+cassandra_host = 'localhost'
+cassandra_keyspace = 'twitter_data'
+cassandra_table = 'tweets'
 
 
-# Function to connect to MySQL database
-def connect_to_mysql():
-    return mysql.connector.connect(host=mysql_host, user=mysql_user, password=mysql_password, database=mysql_database)
+# Function to connect to Cassandra database
+def connect_to_cassandra():
+    cluster = Cluster()
+    return cluster.connect(cassandra_keyspace)
 
 
 # Function to create tweets table if not exists
-def create_tweets_table(cursor):
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS tweets (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        tweet_id BIGINT, 
-        author VARCHAR(255),
-        country VARCHAR(255),
+def create_tweets_table(session):
+    create_table_query = f"""
+    CREATE TABLE IF NOT EXISTS {cassandra_table} (
+        id UUID PRIMARY KEY,
+        tweet_id DECIMAL, 
+        author TEXT,
+        country TEXT,
         content TEXT, 
-        language VARCHAR(10),
-        latitude DECIMAL(10, 6),
-        longitude DECIMAL(10, 6),
+        language TEXT,
         number_of_likes INT,
         number_of_shares INT,
-        date_time DATETIME)
-        """)
-    cursor.execute("TRUNCATE TABLE tweets")
+        date_time TIMESTAMP)
+        """
+    session.execute(create_table_query)
 
 
-# Function to insert tweet into MySQL database
-def insert_tweet_into_mysql(tweet, cursor):
-    query = """
-            INSERT INTO tweets 
-                (tweet_id, author, country, content, language, latitude, longitude, number_of_likes, number_of_shares, date_time) 
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-            """
-    date_time = tweet['date_time']
-    date_time_parse = datetime.datetime.strptime(date_time, '%d/%m/%Y %H:%M')
+# Function to insert tweet into Cassandra database
+def insert_tweet_into_cassandra(tweet, session):
+    insert_query = f"""
+        INSERT INTO {cassandra_table} 
+            (id, tweet_id, author, country, content, language, number_of_likes, number_of_shares, date_time) 
+        VALUES (uuid(), %(tweet_id)s, %(author)s, %(country)s, %(content)s, %(language)s, %(number_of_likes)s, %(number_of_shares)s, %(date_time)s)
+        """
+    date_time_parse = datetime.datetime.strptime(tweet['date_time'], '%d/%m/%Y %H:%M')
     date_time_formatted = date_time_parse.strftime('%Y-%m-%d %H:%M:%S')
 
-    data = (tweet['id'], tweet['author'], tweet['country'], tweet['content'], tweet['language'], tweet['latitude'],
-            tweet['longitude'], tweet['number_of_likes'], tweet['number_of_likes'], date_time_formatted)
-    cursor.execute(query, data)
+    data = {
+        'tweet_id': tweet['id'],
+        'author': tweet['author'],
+        'country': tweet['country'],
+        'content': tweet['content'],
+        'language': tweet['language'],
+        'number_of_likes': tweet['number_of_likes'],
+        'number_of_shares': tweet['number_of_shares'],
+        'date_time': date_time_formatted
+    }
+    session.execute(insert_query, data)
 
 
 # Main function for ingestion service
-def ingest_tweets(csv_file_path, content_filter):
-    db_connection = connect_to_mysql()
-    cursor = db_connection.cursor()
+def ingest_tweets(csv_file_path, author_filter=None, content_filter=None):
+    cluster = connect_to_cassandra()
+    session = cluster
 
     try:
-
         # Create tweets table if not exists
-        create_tweets_table(cursor)
+        create_tweets_table(session)
 
         # Read tweets from CSV file based on filters
         df = pd.read_csv(csv_file_path)
-        df = df.replace({np.nan: None})
+        df = df.replace({pd.NA: None})
+
+        if author_filter:
+            df = df[df['author'].str.contains(author_filter, case=False, na=False)]
+
         if content_filter:
-            filtered_tweets = df[df['content'].str.contains(content_filter, case=False, na=False)]
-        else:
-            filtered_tweets = df
+            df = df[df['content'].str.contains(content_filter, case=False, na=False)]
 
-        # Insert filtered tweets into MySQL
-        for _, tweet in filtered_tweets.iterrows():
-            insert_tweet_into_mysql(tweet, cursor)
+        # Insert filtered tweets into Cassandra
+        for _, tweet in df.iterrows():
+            insert_tweet_into_cassandra(tweet, session)
 
-        db_connection.commit()
         print("Ingestion completed successfully!")
-
     except Exception as e:
         print(f"Error: {str(e)}")
 
     finally:
-        cursor.close()
-        db_connection.close()
+        session.shutdown()
+
 
 
 @app.route('/process_input', methods=['POST'])
@@ -92,14 +95,16 @@ def process_input():
     csv_file_path = 'data/tweets.csv'
     data = request.json
     # Add your processing logic here
-    if 'content_filter' in data.keys():
-        ingest_tweets(csv_file_path, data['content_filter'])
-    else:
-        ingest_tweets(csv_file_path, '')
+    author_filter = data.get('author_filter', None)
+    content_filter = data.get('content_filter', None)
+
+    ingest_tweets(csv_file_path, author_filter, content_filter)
+
     result = {'message': 'Processed successfully'}
     return jsonify(result)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001)
-    # curl -d '{"content_filter":"Israel"}' -H "Content-Type: application/json" -X POST http://localhost:5001/process_input
+    #app.run(host='0.0.0.0', port=5001)
+    csv_file_path = 'data/tweets.csv'
+    ingest_tweets(csv_file_path, '')
