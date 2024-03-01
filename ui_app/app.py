b20f8782ec81a824cb60a57b746258e753f19cf2
GilSemo <gil.semo@darrow.ai>
2024-03-01 15:02:41 +0200

    # Basic ingestion

--- ui_app/app.py
+++ ui_app/app.py
@@ -11,11 +11,17 @@ def home():
 def analytics():
     return render_template('analytics.html')
 
-# @app.route('/search', methods=['POST'])
-# def search():
-#     data = request.json
-#     response = requests.post('http://backend-container:5002/run_query', json={'filters': data['filters']})
-#     return jsonify(response.json())
+@app.route('/ingestion', methods=['GET', 'POST'])
+def ingestion():
+    if request.method == 'GET':
+        return render_template('load-data.html')
+    else:  # Handle the POST request from the ingestion page
+        data = request.json
+        response = requests.post('http://ingestion-container:5001/load_records', json=data)
+        if response.status_code == 200:
+            return jsonify({'message': 'Processed successfully.'}), 200
+        else:
+            return jsonify({'message': 'Failed loading.'}), 500
 
 @app.route('/search', methods=['POST'])
 def search():
@@ -28,10 +34,6 @@ def search():
         return jsonify(response.json()), 200
     else:
         return jsonify({'message': 'Error fetching results.'}), 500
-
-
-
-
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5005)
